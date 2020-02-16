ef3dd34f54cbe5a336671ecdc7cecc7cf201b071
djjtyn <djjtynan@gmail.com>
2020-02-16 17:37:19 +0000

    form working

---
+++ todo/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Item
+from .forms import ItemForm
+
+def get_todo_list(request):
+    results = Item.objects.all()
+    return render (request, 'todo_list.html', {'items': results})
+
+def create_an_item(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect(get_todo_list)
+    else:
+        form = ItemForm()
+    return render(request, 'itemform.html', {'form':form})
